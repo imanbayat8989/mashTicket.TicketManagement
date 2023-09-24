55e51af97ddaf0372f65e165b263381f61fbdee8
iman bayat mokhtari <105519831+imanbayat8989@users.noreply.github.com>
2023-09-24 22:04:47 +0330

    Add project files.

---
+++ mashTicket.TicketManagement.Domain/Entities/Order.cs
@@ -0,0 +1,18 @@
+﻿using mashTicket.TicketManagement.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mashTicket.TicketManagement.Domain.Entities
+{
+    public class Order : AuditbleEntity
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public int OrderTotal { get; set; }
+        public DateTime OrderPlaced { get; set; }
+        public bool OrderPaid { get; set; }
+    }
+}
