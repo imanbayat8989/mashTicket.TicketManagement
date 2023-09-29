5506022c81e556aa0d3af1fc7b35a002ca0d5a01
iman bayat mokhtari <105519831+imanbayat8989@users.noreply.github.com>
2023-09-29 17:35:18 +0330

    09/29/2023 Adding Entity Framework core

---
+++ mashTicket.TicketManagementPersistence/mashTicketDbContext.cs
@@ -0,0 +1,206 @@
+﻿using mashTicket.TicketManagement.Domain.Common;
+using mashTicket.TicketManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mashTicket.TicketManagementPersistence
+{
+    public class mashTicketDbContext : DbContext
+    {
+        public mashTicketDbContext(DbContextOptions<mashTicketDbContext> options) : base(options) 
+        { 
+        }
+
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof (mashTicketDbContext).Assembly);
+
+            var concertGuid = Guid.Parse("{B0788D2F-8003-43C1-92A4-EDC76A7C5DDE}");
+            var musicalGuid = Guid.Parse("{6313179F-7837-473A-A4D5-A5571B43E6A6");
+            var playGuid = Guid.Parse("{BF3F3002-7E53-441E-8B76-F6282BE284AA");
+            var conferenceGuid = Guid.Parse("{FE98F549-3790-4E9F-AA16-18C2292A2EE9");
+
+            modelBuilder.Entity<Category>().HasData(new Category
+            {
+                CategoryId = concertGuid,
+                Name = "Concerts"
+            });
+            modelBuilder.Entity<Category>().HasData(new Category
+            {
+                CategoryId = musicalGuid,
+                Name = "Musicals"
+            });
+            modelBuilder.Entity<Category>().HasData(new Category
+            {
+                CategoryId = playGuid,
+                Name = "Plays"
+            });
+            modelBuilder.Entity<Category>().HasData(new Category
+            {
+                CategoryId = conferenceGuid,
+                Name = "Conferences"
+            });
+
+            modelBuilder.Entity<Event>().HasData(new Event
+            {
+                EventId = Guid.Parse("{EE272F8B-6096-4CB6-8625-BB4BB2D89E8B"),
+                Name = "Bahram Live",
+                Price = 65,
+                Artist = "Bahram",
+                Date = DateTime.Now.AddMonths(6),
+                Description = "Bahram concert baraye avalin bar dar shahr mashhad",
+                ImageUrl = "https://wallpapercave.com/dwp1x/wp3470498.jpg",
+                CategoryId = concertGuid,
+            });
+
+            modelBuilder.Entity<Event>().HasData(new Event
+            {
+                EventId = Guid.Parse("{3448D5A4-0F72-4DD7-BF15-C14A46B26C00"),
+                Name = "Hichkas",
+                Price = 85,
+                Artist = "Hichkas[Sourosh Lashkari]",
+                Date = DateTime.Now.AddMonths(9),
+                Description = "Hichkas Live dar shahre mashhad",
+                ImageUrl = "https://wallpapercave.com/dwp1x/wp4648281.jpg",
+                CategoryId = concertGuid,
+            });
+
+            modelBuilder.Entity<Event>().HasData(new Event
+            {
+                EventId = Guid.Parse("{B419A7CA-3321-4F38-BE8E-4D7B6A529319}"),
+                Name = "Yas",
+                Price = 55,
+                Artist = "Yaser",
+                Date = DateTime.Now.AddMonths(10),
+                Description = "Yas Live dar shahre mashhad",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Yas%2C_Iranian_rapper_live_in_2018_03.jpg/1200px-Yas%2C_Iranian_rapper_live_in_2018_03.jpg?20200909164528",
+                CategoryId = concertGuid,
+            });
+
+            modelBuilder.Entity<Event>().HasData(new Event
+            {
+                EventId = Guid.Parse("{62787623-4C52-43FE-B0C9-B7044FB5929B}"),
+                Name = "Ho3in",
+                Price = 100,
+                Artist = "Ho3in Eblis",
+                Date = DateTime.Now.AddMonths(11),
+                Description = "Ho3in back in the city",
+                ImageUrl = "https://i.pinimg.com/550x/4a/53/f5/4a53f54af90268b764cb54cd749de3dc.jpg",
+                CategoryId = concertGuid,
+            });
+
+            modelBuilder.Entity<Event>().HasData(new Event
+            {
+                EventId = Guid.Parse("{1BABD057-E980-4CB3-9CD2-7FDD9E525668}"),
+                Name = "Sakht Iran 2024 Bad Azadi",
+                Price = 0,
+                Artist = "Jonbesh azady bakhsh Iran",
+                Date = DateTime.Now.AddMinutes(10),
+                Description = "baraye bade azady chegone ba hamkary hamdigar iran azad besazim",
+                ImageUrl = "https://i.pinimg.com/originals/f2/59/82/f2598292d4229b644442cc532f3f88c7.jpg",
+                CategoryId = conferenceGuid,
+            });
+
+            modelBuilder.Entity<Event>().HasData(new Event
+            {
+                EventId = Guid.Parse("{ADC42C09-08C1-4D2C-9F96-2D15BB1AF299}"),
+                Name = "To the Moon and Back",
+                Price = 135,
+                Artist = "Nick Sailor",
+                Date = DateTime.Now.AddMonths(8),
+                Description = "The critics are over the moon and so will you after you've watched this sing and dance extravaganza written by Nick Sailor, the man from 'My dad and sister'.",
+                ImageUrl = "https://gillcleerenpluralsight.blob.core.windows.net/files/GloboTicket/musical.jpg",
+                CategoryId = musicalGuid
+            });
+
+            modelBuilder.Entity<Order>().HasData(new Order
+            {
+                Id = Guid.Parse("{7E94BC5B-71A5-4C8C-BC3B-71BB7976237E}"),
+                OrderTotal = 400,
+                OrderPaid = true,
+                OrderPlaced = DateTime.Now,
+                UserId = Guid.Parse("{A441EB40-9636-4EE6-BE49-A66C5EC1330B}")
+            });
+
+            modelBuilder.Entity<Order>().HasData(new Order
+            {
+                Id = Guid.Parse("{86D3A045-B42D-4854-8150-D6A374948B6E}"),
+                OrderTotal = 135,
+                OrderPaid = true,
+                OrderPlaced = DateTime.Now,
+                UserId = Guid.Parse("{AC3CFAF5-34FD-4E4D-BC04-AD1083DDC340}")
+            });
+
+            modelBuilder.Entity<Order>().HasData(new Order
+            {
+                Id = Guid.Parse("{771CCA4B-066C-4AC7-B3DF-4D12837FE7E0}"),
+                OrderTotal = 85,
+                OrderPaid = true,
+                OrderPlaced = DateTime.Now,
+                UserId = Guid.Parse("{D97A15FC-0D32-41C6-9DDF-62F0735C4C1C}")
+            });
+
+            modelBuilder.Entity<Order>().HasData(new Order
+            {
+                Id = Guid.Parse("{3DCB3EA0-80B1-4781-B5C0-4D85C41E55A6}"),
+                OrderTotal = 245,
+                OrderPaid = true,
+                OrderPlaced = DateTime.Now,
+                UserId = Guid.Parse("{4AD901BE-F447-46DD-BCF7-DBE401AFA203}")
+            });
+
+            modelBuilder.Entity<Order>().HasData(new Order
+            {
+                Id = Guid.Parse("{E6A2679C-79A3-4EF1-A478-6F4C91B405B6}"),
+                OrderTotal = 142,
+                OrderPaid = true,
+                OrderPlaced = DateTime.Now,
+                UserId = Guid.Parse("{7AEB2C01-FE8E-4B84-A5BA-330BDF950F5C}")
+            });
+
+            modelBuilder.Entity<Order>().HasData(new Order
+            {
+                Id = Guid.Parse("{F5A6A3A0-4227-4973-ABB5-A63FBE725923}"),
+                OrderTotal = 40,
+                OrderPaid = true,
+                OrderPlaced = DateTime.Now,
+                UserId = Guid.Parse("{F5A6A3A0-4227-4973-ABB5-A63FBE725923}")
+            });
+
+            modelBuilder.Entity<Order>().HasData(new Order
+            {
+                Id = Guid.Parse("{BA0EB0EF-B69B-46FD-B8E2-41B4178AE7CB}"),
+                OrderTotal = 116,
+                OrderPaid = true,
+                OrderPlaced = DateTime.Now,
+                UserId = Guid.Parse("{7AEB2C01-FE8E-4B84-A5BA-330BDF950F5C}")
+            });
+        
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach (var entry in ChangeTracker.Entries<AuditbleEntity>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedDate = DateTime.Now;
+                        break;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
