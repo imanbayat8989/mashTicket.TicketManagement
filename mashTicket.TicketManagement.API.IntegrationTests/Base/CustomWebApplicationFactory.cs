a7b14a7ad2c9e2f7777f961bb331144e9f6334ef
iman bayat mokhtari <105519831+imanbayat8989@users.noreply.github.com>
2023-10-03 11:28:04 +0330

    10/03/2023 Implementing integration API Tests

---
+++ mashTicket.TicketManagement.API.IntegrationTests/Base/CustomWebApplicationFactory.cs
@@ -0,0 +1,51 @@
+﻿using mashTicket.TicketManagementPersistence;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mashTicket.TicketManagement.API.IntegrationTests.Base
+{
+    public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddDbContext<mashTicketDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("mashTicketDbContextInMemoryTest");
+                });
+                var sp = services.BuildServiceProvider();
+
+                using (var scoped = sp.CreateScope())
+                {
+                    var scopedServices = scoped.ServiceProvider;
+                    var context = scopedServices.GetRequiredService<mashTicketDbContext>();
+                    var logger = scopedServices.GetRequiredService<ILogger<CustomWebApplicationFactory<TStartup>>>();
+
+                    context.Database.EnsureCreated();
+
+                    try
+                    {
+                        Utilities.InitializeDbForTests(context);
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.LogError(ex, $"An error occurred seeding the database with test messages. Error: {ex.Message}");
+                    }
+                };
+            });
+        }
+        public HttpClient GetAnonymousClient()
+        {
+            return CreateClient();
+        }
+    }
+}
